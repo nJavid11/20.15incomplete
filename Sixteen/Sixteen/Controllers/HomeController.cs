5d62cf1d643265a02eedcf6df96929a0e3a24667
nJavid11 <tu201803071@code.edu.az>
2021-12-20 01:48:38 +0400

    incomplete

---
+++ Sixteen/Sixteen/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sixteen.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sixteen.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
